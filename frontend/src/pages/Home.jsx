5462ede0e8c84fcc3bca96237c67a87b1fb2c54a
supuni <sitharabandara1997@gmail.com>
2024-07-19 19:29:25 +0530

    added aome styles to the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -49,6 +49,18 @@ const Home = () => {
       </Container>
     </section>
     {/* hero end*/}
+
+    <section>
+      <Container>
+        <Row>
+          <Col lg='3'>
+          <h5 className='services__subtitle'>What we serve</h5>
+          <h2 className='services__title'>We offer our best services</h2>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
     </>
   )
 }

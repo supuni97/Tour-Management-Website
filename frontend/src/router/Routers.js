872fd22972d8695814ec2c6b918f30ab46d924d3
supuni <sitharabandara1997@gmail.com>
2024-07-19 11:05:03 +0530

    Added routes

--- frontend/src/router/Routers.js
+++ frontend/src/router/Routers.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {Routes, Route, Navigate} from 'react-router-dom';
+import Home from './../pages/Home';
+import Tours from './../pages/Tours';
+import Login from './../pages/Login';
+import Register from './../pages/Register';
+import TourDetails from './../pages/TourDetails';
+import SearchResultList from './../pages/SearchResultList';
 
 const Routers = () => {
   return (
-    <div>Routers</div>
+    <Routes>
+    <Route path='/' element={<Navigate to='/home' />} />
+    <Route path='/home' element = {<Home />} />
+    <Route path='/tours' element = {<Tours />} />
+    <Route path='/tours/:id' element = {<TourDetails />} />
+    <Route path='/login' element = {<Login />} />
+    <Route path='/register' element = {<Register />} />
+    <Route path='/tours/search' element = {<SearchResultList />} />
+
+    </Routes>
   )
 }
 

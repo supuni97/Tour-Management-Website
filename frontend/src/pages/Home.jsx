23018b652d624ec99a99157b903c1e845bcb5be5
supuni <sitharabandara1997@gmail.com>
2024-07-19 18:16:16 +0530

    Added home page basic elements

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React from 'react';
+import '../styles/home.css';
+import { Container, Row, Col } from 'reactstrap';
+import Subtitle from './../shared/Subtitle';
+import heroImg from '../assets/images/hero-img01.jpg';
+import heroImg02 from '../assets/images/hero-img02.jpg';
+import heroVidep from '../assets/images/hero-video.mp4';
+import WorldImg from '../assets/images/world.png';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    {/* hero start */}
+    <section>
+      <Container>
+      <Row>
+        <Col lg='6'>
+        <div className='hero__content'>
+        <div className='hero__subtitle d-flex align-items-center'>
+          <Subtitle subtitle={'Know Before You Go'} />
+          <img src={WorldImg} alt='' />
+        </div> 
+        <h1>Traveling opens the door to creating <span className='highlight'>memories</span> </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        </Col>
+        <Col lg='2'>
+        <div className='hero__img-box'>
+        <img src={heroImg} alt='' />
+
+        </div>
+        </Col>
+        <Col lg='2'>
+        <div className='hero__img-box mt-4'>
+        <video src={heroVidep} alt='' controls />
+
+        </div>
+        </Col> <Col lg='2'>
+        <div className='hero__img-box mt-5'>
+        <img src={heroImg02} alt='' />
+
+        </div>
+        </Col>
+      </Row>
+      </Container>
+    </section>
+    {/* hero end*/}
+    </>
   )
 }
 

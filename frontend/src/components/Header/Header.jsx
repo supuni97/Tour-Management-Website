fa93eedd1d9e737c6a0739ce89685fb8d03c02b4
Supuni Sithara <sitharabandara1997@gmail.com>
2024-08-07 21:26:55 +0530

    Completed footer

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useRef,useEffect} from 'react';
 import { Container, Row, Button } from 'reactstrap';
 import { NavLink, Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
@@ -20,8 +20,28 @@ const nav__links = [
 ];
 
 const Header = () => {
+
+const headerRef = useRef(null)
+
+const stickyHeaderFUnc = () => {
+window.addEventListener('scroll',()=>{
+  if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+    headerRef.current.classList.add('sticky__header')
+  }else{
+    headerRef.current.classList.remove('sticky__header')
+
+  }
+})
+}
+
+useEffect(()=>{
+  stickyHeaderFUnc()
+
+  return window.removeEventListener('scroll',stickyHeaderFUnc)
+})
+
   return (
-    <header className='header'>
+    <header className='header' ref={headerRef}>
       <Container>
         <Row>
           <div className='nav__wrapper d-flex align-items-center justify-content-between'>

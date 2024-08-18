92bc99df956e10024c3ca49e3d879573cc9dfdee
Supuni Sithara <sitharabandara1997@gmail.com>
2024-08-18 21:14:31 +0530

    Added user routes and api end points

---
+++ backend/routes/users.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {deleteUser, getAllUser, getSIngleUser, updateUser } from '../controllers/userController';
+
+const router = express.Router();
+
+//update user
+router.put("/:id",updateUser);
+
+//delete user
+router.delete("/:id",deleteUser);
+
+//get single user
+router.get("/:id",getSIngleUser);
+
+//get all users
+router.get("/",getAllUser);
+
+export default router;

5302862fa6a9ef153fe95f5e97498d69ae15c266
Supuni Sithara <sitharabandara1997@gmail.com>
2024-08-22 20:46:28 +0530

    Connected fontend with backend for register page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+  <AuthContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </AuthContextProvider>
   </React.StrictMode>
 );

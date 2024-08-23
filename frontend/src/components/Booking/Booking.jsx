6cd104965d0bddd91c787bde110dea51c284015f
Supuni Sithara <sitharabandara1997@gmail.com>
2024-08-23 18:06:26 +0530

    fetched data to booking and tour details

--- frontend/src/components/Booking/Booking.jsx
+++ frontend/src/components/Booking/Booking.jsx
@@ -1,35 +1,63 @@
-import React,{useState} from 'react';
+import React, { useState, useContext } from 'react';
 import './booking.css';
 import { Form, FormGroup, ListGroup, ListGroupItem, Button } from 'reactstrap';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
+import { BASE_URL } from '../../utils/config';
 
 const Booking = ({ tour, avgRating }) => {
 
-    const { price, reviews } = tour;
-
+    const { price, reviews, title } = tour;
     const navigate = useNavigate();
+    const { user } = useContext(AuthContext)
 
-    const [credentials, setCredentials] =useState ({
-        userId:'01', //later will be dynamic
-        userEmail:'example@mail.com',
-        userName:'',
-        phone:'',
-        guestSize:1,
-        bookAt:''
+    const [booking, setBooking] = useState({
+        userId: user && user._id,
+        userEmail: user && user.email,
+        tourName: title,
+        phone: '',
+        guestSize: 1,
+        bookAt: ''
     })
 
     const handleChange = e => {
-        setCredentials(prev=>({...prev,[e.target.id]: e.target.value}))
+        setBooking(prev => ({ ...prev, [e.target.id]: e.target.value }))
     };
 
     const serviceFee = 10
-    const totalAmount = Number(price) * Number(credentials.guestSize) + Number(serviceFee);
+    const totalAmount = Number(price) * Number(booking.guestSize) + Number(serviceFee);
 
     //send data to the server
-    const handleClick = e =>{
+    const handleClick = async e => {
         e.preventDefault();
 
-       navigate('/thank-you');
+        console.log(booking)
+
+        try {
+            if (!user || user === undefined || user === null) {
+                return alert('Please sign in')
+            }
+
+            const res = await fetch(`${BASE_URL}/booking`, {
+                method: 'post',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                credentials: 'include',
+                body: JSON.stringify(booking)
+            })
+
+            const result = await res.json();
+            if (!res.ok) {
+                return alert(result.message);
+            }
+            navigate('/thank-you');
+           
+        } catch (err) {
+            alert(err.message);
+        }
+
+        
     }
 
 
@@ -42,7 +70,7 @@ const Booking = ({ tour, avgRating }) => {
                     {avgRating === 0 ? null : avgRating} ({reviews?.length})
                 </span>
             </div>
-    
+
             {/*booking form start */}
             <div className='booking_form'>
                 <h5>Information</h5>
@@ -54,7 +82,7 @@ const Booking = ({ tour, avgRating }) => {
                         <input type='number' placeholder='Phone' id='phone' required onChange={handleChange} />
                     </FormGroup>
                     <FormGroup className='d-flex align-items-center gap-3'>
-                        
+
                         <input type='date' placeholder='' id='bookAt' required onChange={handleChange} />
                         <input type='number' placeholder='Guest' id='guestSize' required onChange={handleChange} />
 
@@ -67,19 +95,19 @@ const Booking = ({ tour, avgRating }) => {
             <div className='booking_btm'>
                 <ListGroup>
                     <ListGroupItem className='border-0 px-0'>
-                    <h5 className='d-flex align-items-center gap-1'>${price}<i class="ri-close-line"></i> 1 person</h5>
-                    <span>${price}</span>
+                        <h5 className='d-flex align-items-center gap-1'>${price}<i class="ri-close-line"></i> 1 person</h5>
+                        <span>${price}</span>
                     </ListGroupItem>
                     <ListGroupItem className='border-0 px-0'>
-                    <h5>Service charge</h5>
-                    <span>${serviceFee}</span>
+                        <h5>Service charge</h5>
+                        <span>${serviceFee}</span>
                     </ListGroupItem>
                     <ListGroupItem className='border-0 px-0 total'>
-                    <h5>Total</h5>
-                    <span>${totalAmount}</span>
+                        <h5>Total</h5>
+                        <span>${totalAmount}</span>
                     </ListGroupItem>
                 </ListGroup>
-                <Button  className='btn primary__btn w-100 mt-4' onClick={handleClick}>Book Now</Button>
+                <Button className='btn primary__btn w-100 mt-4' onClick={handleClick}>Book Now</Button>
             </div>
 
         </div>
--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useContext, useRef, useState } from 'react';
 import '../styles/tour-details.css';
-import { Container, Row, Col, Form, ListGroup, ListGroupItem } from 'reactstrap';
+import { Container, Row, Col, Form, ListGroup } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import calculateAvgRating from '../utils/avgRating';
 import avatar from '../assets/images/avatar.jpg';
@@ -8,12 +8,15 @@ import Booking from '../components/Booking/Booking';
 import useFetch from '../hooks/useFetch';
 import { BASE_URL } from '../utils/config';
 import Newsletter from '../shared/Newsletter';
+import { AuthContext } from '../context/AuthContext';
+
 const TourDetails = () => {
 
   const { id } = useParams();
 
   const reviewMsgRef = useRef('')
   const [tourRating, setTourRating] = useState(null)
+  const { user } = useContext(AuthContext);
 
   //fetch data from API
   const { data: tour, loading, error } = useFetch(`${BASE_URL}/tours/${id}`)
@@ -27,9 +30,41 @@ const TourDetails = () => {
   const options = { day: "numeric", month: "long", year: "numeric" };
 
   //submit request to server
-  const submitHandler = e => {
+  const submitHandler = async e => {
     e.preventDefault()
     const reviewText = reviewMsgRef.current.value;
+
+    try {
+
+      if (!user || user === undefined || user === null) {
+        alert('Please sign in')
+      }
+
+      const reviewObj = {
+        username: user?.username,
+        reviewText,
+        reting: tourRating
+      }
+
+      const res = await fetch(`${BASE_URL}/review/${id}`, {
+        method: 'post',
+        headers: {
+          'content-type': 'application/json'
+        },
+        credentials: 'include',
+        body: JSON.stringify(reviewObj)
+      })
+
+      const result = await res.json();
+      if(!res.ok) {
+        return alert(result.message);
+      }
+      alert(result.message)
+       
+    } catch (err) {
+      alert(err.message);
+    }
+
   }
 
   useEffect(() => {
@@ -118,14 +153,14 @@ const TourDetails = () => {
                             <div className='w-100'>
                               <div className='d-flex align-items-center justify-content-between'>
                                 <div>
-                                  <h5>Kamal</h5>
-                                  <p>{new Date('01-20-2024').toLocaleDateString("en-US", options)}</p>
+                                  <h5>{review.username}</h5>
+                                  <p>{new Date(review.createdAt).toLocaleDateString("en-US", options)}</p>
                                 </div>
                                 <span className='d-flex align-items-center'>
-                                  5<i class="ri-star-s-fill"></i>
+                                  {review.rating}<i class="ri-star-s-fill"></i>
                                 </span>
                               </div>
-                              <h6>Amazing tour.</h6>
+                              <h6>{review.reviewText}</h6>
                             </div>
                           </div>
                         ))
